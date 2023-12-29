4c949dd848e47c692171efe0d6bc94631ef2049c
KIRAN DONGRE <140075470+kirandongre001@users.noreply.github.com>
2023-12-29 20:05:25 +0530

    Add files via upload

---
+++ ProductMVC/BLL/ProductManager.cs
@@ -0,0 +1,17 @@
+﻿namespace BLL;
+
+using BOL;
+
+using DAL;
+
+public class ProductManager
+{
+    public List<Product>showAllPro()
+    {
+        List<Product>allProducts=new List<Product>();
+
+        allProducts=DBManager.showAllPro();
+        return allProducts;
+    }
+
+}
